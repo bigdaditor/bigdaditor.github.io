4c983c10cc381f7d1711c9d769a4cb7107f7e4ef
bigdaditor <bigdaditor1015@gmail.com>
2025-08-06 20:10:32 +0900

    1. 인트로 문구 추가

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const terminal = document.querySelector(".terminal");
   const commands = {
@@ -11,6 +9,28 @@ document.addEventListener("DOMContentLoaded", () => {
       "Stay tuned!"
     ].join("\n")
   };
+
+  function createBlogIntro() {
+    const intro = document.createElement("div");
+    intro.className = "blog-intro";
+
+    const pre = document.createElement("pre");
+    pre.className = "ascii-welcome";
+    pre.textContent = `
+__        __   _                            _         
+\\ \\      / /__| | ___ ___  _ __ ___   ___  | |_ ___    
+ \\ \\ /\\ / / _ \\ |/ __/ _ \\| '_ \` _ \\ / _ \\ | __/ _ \\   
+  \\ V  V /  __/ | (_| (_) | | | | | |  __/ | || (_) |  
+   \\_/\\_/ \\___|_|\\___\\___/|_| |_| |_|\\___|  \\__\\___/   
+`;
+
+    const desc = document.createElement("p");
+    desc.innerHTML = 'This is the terminal-style blog of <strong>bigdaditor</strong>. Type <code>help</code> to get started!';
+
+    intro.appendChild(pre);
+    intro.appendChild(desc);
+    terminal.appendChild(intro);
+  }
 
   function createPromptLine() {
     const line = document.createElement("div");
@@ -78,5 +98,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // Start terminal
   terminal.innerHTML = "";
+  createBlogIntro();
   createPromptLine();
 });
